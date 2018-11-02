e073bd268d13fae5d871b153dcc0d111d486474a
Lin, Bernard <bernardlin@outlook.com>
2018-11-02 16:00:52 -0400

    check in

---
+++ src/constants/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: "rebloc-dev.auth0.com",
+  roleUrl: "https://rbac-tutorial-app/role",
+  clientId: "n_NYaMP99VKAAttMxsyKCvMsZG98kYSn",
+  callbackUrl: "http://localhost:3000/callback"
+};
