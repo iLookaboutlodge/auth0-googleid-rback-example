d95b2461c1bf1dbd0fed5b9a66efaaa71c016221
Lin, Bernard <bernardlin@outlook.com>
2018-11-16 09:50:00 -0500

    update logout code

--- src/constants/auth0-variables.js
+++ src/constants/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: "rebloc.auth0.com",
-  roleUrl: "https://rbac-tutorial-app/role",
+  roleUrl: "https://demo-app.rebloc.io/roles",
   clientId: "fnLCIbVoycyrbNwmip5aWFLEtAyfdSYF",
   callbackUrl: "http://demo-app.rebloc.io:3000/callback"
 };
