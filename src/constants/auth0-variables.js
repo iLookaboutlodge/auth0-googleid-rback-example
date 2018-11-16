a8e99cfc2cfd940bf864d2dce0ab2a3b75a53768
Lin, Bernard <bernardlin@outlook.com>
2018-11-16 09:51:40 -0500

    Revert "update logout code"
    This reverts commit d95b2461c1bf1dbd0fed5b9a66efaaa71c016221.

--- src/constants/auth0-variables.js
+++ src/constants/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: "rebloc.auth0.com",
-  roleUrl: "https://demo-app.rebloc.io/roles",
+  roleUrl: "https://rbac-tutorial-app/role",
   clientId: "fnLCIbVoycyrbNwmip5aWFLEtAyfdSYF",
   callbackUrl: "http://demo-app.rebloc.io:3000/callback"
 };
